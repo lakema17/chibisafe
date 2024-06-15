6ee77e5d33ef2737846c2ba50562e76ec1055ad7
Lake <18357858+lakema17@users.noreply.github.com>
2024-06-15 09:51:48 -0700

    Update shiki.ts to support Caddyfiles

--- packages/next/src/lib/shiki.ts
+++ packages/next/src/lib/shiki.ts
@@ -1,6 +1,11 @@
 import { bundledLanguages, getHighlighter } from 'shiki';
 
+const caddyLang = JSON.parse(fs.readFileSync('caddyfile.tmLanguage.json', 'utf8'))
+
 export const highlighter = await getHighlighter({
 	themes: ['github-dark-dimmed'],
-	langs: Object.keys(bundledLanguages)
+	langs: [caddyLang, ...Object.keys(bundledLanguages)],
+  langAlias: {
+    caddyLang: 'Caddyfile'
+  }
 });
